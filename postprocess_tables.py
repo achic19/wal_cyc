7f37b0c9f8de391ad2d68d90bd1386e026943f40
achic19 <38861334+achic19@users.noreply.github.com>
2022-01-09 12:53:48 +0200

    4326 crs

--- postprocess_tables.py
+++ postprocess_tables.py
@@ -20,17 +20,20 @@ def grant_select_permissions_to_guest(engine):
 
 
 def create_wkt_and_index(engine):
+    name_table_to_update = ['car_count_data', 'cycle_count_data', 'incident_data', 'openstreetmap_road_network']
     inspector = inspect(engine)
     for table_name in inspector.get_table_names(schema=PRODUCTION_SCHEME):
-        table = gpd.GeoDataFrame.from_postgis(f'select * from {PRODUCTION_SCHEME}.{table_name}', engine,
-                                              geom_col='geometry')
-        if 'wkt' not in table.columns:
-            table['wkt'] = table.geometry.astype(str)
-            munich.MunichData.data_to_server(table, table.columns.to_list(), table_name, engine)
-        if not is_table_has_spatial_index(table_name, inspector):
-            with engine.connect() as connection:
-                result = connection.execute(
-                    text(f"CREATE INDEX ON {PRODUCTION_SCHEME}.{table_name} using gist (geometry)"))
+        if table_name in name_table_to_update:
+            table = gpd.GeoDataFrame.from_postgis(f'select * from {PRODUCTION_SCHEME}.{table_name}', engine,
+                                                  geom_col='geometry')
+            table.to_crs(crs="EPSG:4326", inplace=True)
+            if 'wkt' not in table.columns:
+                table['wkt'] = table.geometry.astype(str)
+                munich.MunichData.data_to_server(table, table.columns.to_list(), table_name, engine)
+            if not is_table_has_spatial_index(table_name, inspector):
+                with engine.connect() as connection:
+                    result = connection.execute(
+                        text(f"CREATE INDEX ON {PRODUCTION_SCHEME}.{table_name} using gist (geometry)"))
 
 
 if __name__ == '__main__':
